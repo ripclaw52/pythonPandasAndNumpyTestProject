670c0993f254f9b4ec3451ddb5cc68c9d91f096a
aaron2708 <aaronalejo2708@gmail.com>
2022-11-22 18:40:01 -0700

    Integrated map into Dash

--- map-files/edmonton-map.py
+++ map-files/edmonton-map.py
@@ -3,59 +3,97 @@ import json
 import pandas as pd
 import plotly.express as px
 
-# Opens the downloaded Neighbourhoods geojson file
-with open('City of Edmonton - Neighbourhoods.geojson', 'r') as f:
-    neighbourhood = json.load(f)
+#======================================================================================================================#
 
-'''
-Feature IDs are required in GeoJSON, the Edmonton GeoJSON has the neighbourhood ID in the properties keys 
-(the loaded geojson if basically a big dictionary so access values treat it like a dict). This code assigns each
-neighbourhood with its respective ID. This for loop adds an ID key in the neighbourhood geojson with the 
-neighbourhood's ID.
-'''
-# neighbourhoodID_List = []
-i=0
-for feature in neighbourhood["features"]:
-    feature['id'] = int(neighbourhood["features"][i]['properties']['neighbourhood_number'])
-    # neighbourhoodID_List.append(int(neighbourhood["features"][i]['properties']['neighbourhood_number']))
-    i += 1
-    # print(int(neighbourhood["features"][i]['properties']['neighbourhood_number']))
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H4('Political candidate voting pool analysis'),
+    html.P("Select a candidate:"),
+    dcc.RadioItems(
+        id='candidate',
+        options=["ERMINESKIN", "MACEWAN", "DOWNTOWN"],
+        value="DUGGAN",
+        inline=True
+    ),
+    dcc.Graph(id="graph"),
+])
 
 
-# print(counties["features"][0])
-# print(int(neighbourhood["features"][0]['properties']['neighbourhood_number']))
-# print(neighbourhood["features"][0]['id'])
-# print(neighbourhood["features"][0])
+@app.callback(
+    Output("graph", "figure"),
+    Input("candidate", "value"))
+def display_choropleth(candidate):
+    # df = px.data.election() # replace with your own data source
+    # geojson = px.data.election_geojson()
+    # fig = px.choropleth(
+    #     df, geojson=geojson, color=candidate,
+    #     locations="district", featureidkey="properties.district",
+    #     projection="mercator", range_color=[0, 6500])
+    # fig.update_geos(fitbounds="locations", visible=False)
+    # fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    #
+    #==================================================================================================================#
+    # Opens the downloaded Neighbourhoods geojson file
+    with open('City of Edmonton - Neighbourhoods.geojson', 'r') as f:
+        neighbourhood = json.load(f)
 
-df1 = pd.read_csv("2016_Census_-_Dwelling_Unit_by_Language__Neighbourhood_Ward_.csv")
-#df2 = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
+    '''
+    Feature IDs are required in GeoJSON, the Edmonton GeoJSON has the neighbourhood ID in the properties keys 
+    (the loaded geojson if basically a big dictionary so access values treat it like a dict). This code assigns each
+    neighbourhood with its respective ID. This for loop adds an ID key in the neighbourhood geojson with the 
+    neighbourhood's ID.
+    '''
+    # neighbourhoodID_List = []
+    i = 0
+    for feature in neighbourhood["features"]:
+        feature['id'] = int(neighbourhood["features"][i]['properties']['neighbourhood_number'])
+        # neighbourhoodID_List.append(int(neighbourhood["features"][i]['properties']['neighbourhood_number']))
+        i += 1
+        # print(int(neighbourhood["features"][i]['properties']['neighbourhood_number']))
 
-df1.head()
-# print(df1.NeighbourhoodID)
-# column_headers = list(df2.columns.values)
-# print("The Column Header :", df1.iloc[[0]])
+    # print(counties["features"][0])
+    # print(int(neighbourhood["features"][0]['properties']['neighbourhood_number']))
+    # print(neighbourhood["features"][0]['id'])
+    # print(neighbourhood["features"][0])
 
-''' Sets up the choropleth map of Edmonton.
-- geojson=neighbourhood, where neighbourhood is the loaded json from the GeoJSON file. The neighbourhood ID that was 
-created in the for loop matches the neighbourhood ID in the dataframe column which is named 'NeighbourhoodID' in the
-csv file.
+    df1 = pd.read_csv("2016_Census_-_Dwelling_Unit_by_Language__Neighbourhood_Ward_.csv")
+    # df2 = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
 
-- locations=df1.NeighbourhoodID, where df1.NeighbourhoodID accesses the column NeighbourhoodID in the CSV file. df1 is 
-the data frame for the language by neighbourhood dataset, but we could use  different or multiple dataset (will have to 
-merge the datasets if multiple were used, idk how to do this yet). To access columns in the dataFrame 
+    df1.head()
+    # print(df1.NeighbourhoodID)
+    # column_headers = list(df2.columns.values)
+    # print("The Column Header :", df1.iloc[[0]])
 
-Helpful Resources:
-- https://medium.com/tech-carnot/interactive-map-based-visualization-using-plotly-44e8ad419b97
-- https://plotly.com/python/mapbox-county-choropleth/
-- This-> https://stackoverflow.com/questions/60815784/map-with-choroplethmapbox-isnt-showing-in-dash 
-  Then -> https://community.plotly.com/t/plot-a-shapefile-shp-in-a-choropleth-chart/27850
-'''
-fig = px.choropleth_mapbox(df1, geojson=neighbourhood, locations=df1.NeighbourhoodID, color=df1.Ward,
-                           #color_continuous_scale="Viridis",
-                           mapbox_style="carto-positron",
-                           zoom=10, center={"lat": 53.545883, "lon": -113.490112},
-                          )
+    ''' Sets up the choropleth map of Edmonton.
+    - geojson=neighbourhood, where neighbourhood is the loaded json from the GeoJSON file. The neighbourhood ID that was 
+    created in the for loop matches the neighbourhood ID in the dataframe column which is named 'NeighbourhoodID' in the
+    csv file.
 
-fig.update_geos(fitbounds="locations", visible=False)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+    - locations=df1.NeighbourhoodID, where df1.NeighbourhoodID accesses the column NeighbourhoodID in the CSV file. df1 is 
+    the data frame for the language by neighbourhood dataset, but we could use  different or multiple dataset (will have to 
+    merge the datasets if multiple were used, idk how to do this yet). To access columns in the dataFrame 
+
+    Helpful Resources:
+    - https://medium.com/tech-carnot/interactive-map-based-visualization-using-plotly-44e8ad419b97
+    - https://plotly.com/python/mapbox-county-choropleth/
+    - This-> https://stackoverflow.com/questions/60815784/map-with-choroplethmapbox-isnt-showing-in-dash 
+      Then -> https://community.plotly.com/t/plot-a-shapefile-shp-in-a-choropleth-chart/27850
+    '''
+    fig = px.choropleth_mapbox(df1, geojson=neighbourhood, locations=df1.NeighbourhoodID,
+                               color=df1.NeighbourhoodName==candidate,
+                               mapbox_style='open-street-map',
+                               zoom=10, center={"lat": 53.545883, "lon": -113.490112},
+                               labels=df1.NeighbourhoodName,
+                               )
+
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(coloraxis_showscale=False, showlegend=None )
+    #fig.show()
+    return fig
+
+
+app.run_server(debug=True)
